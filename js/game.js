9c802e9f0635f896ac748ed5dc60f684366c7f8a
Justin LaRose <justinplarose@gmail.com>
2018-04-06 13:06:36 -0400

    Setting Controller Back to Beginning

--- js/game.js
+++ js/game.js
@@ -112,34 +112,16 @@ var waititsfun = (function () {
 
             lilly.body.velocity.x = 0;
 
-            var gpad = gamepad.GetRightStick();
-            if(gpad && gpad.direction !== 0){
-                if(gpad.direction === 8){
-                    lilly.body.velocity.x = 450;
-                }else if(gpad.direction === 16){
-                    lilly.body.velocity.x = -450;
-                }
-            }else{
-            if (cursors.left.isDown) {
-                // lilly.body.velocity.x = -450;
-                lilly.body.velocity.x = 500;
-            }
-            else if (cursors.right.isDown) {
-                // lilly.body.velocity.x = 450;
-                lilly.body.velocity.x = -1000;
-            }else if (Phaser.Device.touch) {
-                if (game.input.pointer1.isDown) {
-                    lilly.body.x = game.input.pointer1.x;
-                } else {
+
                     if (cursors.left.isDown) {
                         lilly.body.velocity.x = -450;
                     }
                     else if (cursors.right.isDown) {
                         lilly.body.velocity.x = 450;
                     }
-                }
-            }
-        }
+                
+            
+        
 
         }
     };
